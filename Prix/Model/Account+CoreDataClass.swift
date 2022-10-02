18316864ec25c08495a3ffed9e89694c3f6a71b4
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-10-02 20:54:41 +0300

    Prepare core data model

---
+++ Prix/Model/Account+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Account+CoreDataClass.swift
+//  Prix
+//
+//  Created by newbie on 02.10.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Account: NSManagedObject {
+
+}
---
+++ Prix/Model/Goal+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Goal+CoreDataClass.swift
+//  Prix
+//
+//  Created by newbie on 02.10.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Goal: NSManagedObject {
+
+}
